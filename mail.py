ee1573a7dda09f39789f9952d4972d47a2f6618a
New-Alex <rocfor444@gmail.com>
2025-12-23 11:51:26 +0100

    цыклы

--- mail.py
+++ mail.py
@@ -1,4 +1,34 @@
+
+
+
+r=['erfer', "34", 'deswd']
+text = ['34', 'd34', '345']
+
+def fire(text):
+    for item in text:
+        if item[0] == '3':
+            print('мы нашли элемент 3')
+        elif item[0] == 'd':
+            print('мы нашли элемент d')
+        else:
+            print('мы нашли элемент который не подходит под условия')
+
+
+data = [
+    {"name": "Alex", "age": 25, "job": "dev"},
+    {"name": "Max", "age": 36, "job": "front"},
+    {"name": "Maximka", "age": 19, "job": "front"}
+]
+
+def getData(dataList):
+    for element in dataList:
+        if element['name'] == "Alex":
+            print("привет Алекс")
+        elif element['age'] > 27:
+            print(f'уважаемый {element["name"]} вам уже {element["age"]} лет вы нам не подходите')
+        if element['job'] == "dev":
+            print("Пока")
 if __name__=="__main__":
-    print("Enter the number of elements in the array")
-    print("ok "*10)
-    print("ffdfdsdfs")
+    
+    fire(text)
+  
